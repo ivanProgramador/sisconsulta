56542702522ef77cb13b7aa252118341b4ef3ce8
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-11-14 11:54:11 -0300

    seed de empresas  funcionando

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use PhpParser\Node\Stmt\Echo_;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $companies = [];
+
+        for ($index = 1; $index <= 3; $index++) {
+            $companies[] = [
+                'company_name'=>'Empresa ' . $index,
+                'company_logo'=>'empresa_0' . $index .'.png',
+                'uuid'=> Str::uuid(),
+                'address'=>'Rua da empresa'.$index.', 123,Bairro Exemplo, Cidade Exemplo',
+                'phone'=> '89774455'. $index,
+                'email'=>'empresa'.$index.'@localhost.com',
+                'status'=> 'active',
+                'created_at'=> now(),
+                'updated_at'=> now(),   
+                'deleted_at'=> null,
+            ];
+        }
+
+        DB::table('companies')->insert($companies);
+
+        echo count($companies)."3 empresas cadastadas com sucesso !.\n";
+    }
+}
