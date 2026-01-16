71e4d1f4f937649a9c162b55821e20dd73a10fa3
Ivan Rodrigues <ivan-guita@hotmail.com>
2026-01-16 13:43:43 -0300

    correcao view dispensador

--- resources/views/ticket_dispenser/dispenser.blade.php
+++ resources/views/ticket_dispenser/dispenser.blade.php
@@ -1,16 +1,11 @@
 <x-layouts.guest-layout subtitle="{{ empty($subtitle) ? '' : $subtitle }}">
 
     <div class="flex flex-col justify-center p-4">
-<<<<<<< HEAD
-=======
-        
->>>>>>> 6b2be7255ce87336a28778bdcdf6b108ec7b81e7
         <div class="text-end mb-4">
             [opções]
         </div>
 
         <div class="main-card flex gap-4 w-full">
-<<<<<<< HEAD
 
             <div class="flex flex-wrap w-full border-1 border-slate-300 rounded-xl">
 
@@ -19,53 +14,44 @@
                         Fila de espera 1 
                     </div>
                 </div>
-                 <div class="w-1/4 p-4">
-                    <div class="main-card">
-                        Fila de espera 2
-                    </div>
-                </div>
-                 <div class="w-1/4 p-4">
-                    <div class="main-card">
-                        Fila de espera 3
-                    </div>
-                </div>
 
-                 <div class="w-1/4 p-4">
+                <div class="w-1/4 p-4">
                     <div class="main-card">
                         Fila de espera 1 
                     </div>
                 </div>
-                 <div class="w-1/4 p-4">
-                    <div class="main-card">
-                        Fila de espera 2
-                    </div>
-                </div>
-                 <div class="w-1/4 p-4">
-                    <div class="main-card">
-                        Fila de espera 3
-                    </div>
-                </div>
 
-                 <div class="w-1/4 p-4">
+                <div class="w-1/4 p-4">
                     <div class="main-card">
                         Fila de espera 1 
                     </div>
                 </div>
-                 <div class="w-1/4 p-4">
+                <div class="w-1/4 p-4">
                     <div class="main-card">
-                        Fila de espera 2
+                        Fila de espera 1 
                     </div>
                 </div>
-                 <div class="w-1/4 p-4">
+                <div class="w-1/4 p-4">
                     <div class="main-card">
-                        Fila de espera 3
+                        Fila de espera 1 
                     </div>
                 </div>
-
-
-       
-               
-                 
+                <div class="w-1/4 p-4">
+                    <div class="main-card">
+                        Fila de espera 1 
+                    </div>
+                </div>
+                <div class="w-1/4 p-4">
+                    <div class="main-card">
+                        Fila de espera 1 
+                    </div>
+                </div>
+                <div class="w-1/4 p-4">
+                    <div class="main-card">
+                        Fila de espera 1 
+                    </div>
+                </div>
+                  
             </div>
 
             
@@ -74,32 +60,12 @@
                 </div>
                      
         
-=======
-             <div class="flex flex-wrap w-full border-1 border-slate-300 rounded-xl">
-                 <div class="w-1/2 p-4">
-                     <div class="main-card">
-                          Fila de espera 1
-                     </div>
-                     <div class="main-card">
-                          Fila de espera 2
-                     </div>
-                     <div class="main-card">
-                          Fila de espera 3
-                     </div>
-                 </div>
-
-             </div>
->>>>>>> 6b2be7255ce87336a28778bdcdf6b108ec7b81e7
         </div>
 
 
 
-<<<<<<< HEAD
 
 
     </div>
 
-=======
-    </div>
->>>>>>> 6b2be7255ce87336a28778bdcdf6b108ec7b81e7
 </x-layouts.guest-layout>
