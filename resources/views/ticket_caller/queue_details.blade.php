4554c7dabe5f237ef6763aee690c3750fa73ff6c
Ivan Rodrigues <ivan-guita@hotmail.com>
2026-03-11 14:58:12 -0300

    mostrador de numeros das senhas pronto

--- resources/views/ticket_caller/queue_details.blade.php
+++ resources/views/ticket_caller/queue_details.blade.php
@@ -11,27 +11,43 @@
           <p class="title-3" >Balcão: <strong>{{ $queue->service_desk}}</strong></p>
        </div>
 
-      <div class="w-1/4 rouded-xl border-1 border-slate-400 text-center p-4" >
-         <div class="flex gap-6 mb-4">
-            <p class="title-3">Última senha chamada:</p>
-            @if(empty($lastTicket))
-               <p class="text-slate-400 my-4">Não existe senha anterior</p>
-            @else 
-               <p class="text-6xl font-bold">{{ $lastTicket->queue_ticket_number }}</p>
-            @endif
-            </div>
-      </div>
 
-      <div class="w-1/4 rouded-xl border-1 border-slate-400 text-center p-4">
-            <div class="flex gap-6 mb-4">
-            <p class="title-3">Próxima senha:</p>
-            @if(empty($nextTicket)) 
-               <p class="text-slate-400 my-4">Não existe próxima senha</p>
-            @else 
-               <p class="text-6xl font-bold">{{ $nextTicket->queue_ticket_number }}</p>
-            @endif
-            </div>
-       </div>
+               <div class="flex justify-center gap-6">
+                  
+                     <div class="w-1/4 rounded-xl border-1 border-slate-400 text-center p-4" >
+                        <div class="flex gap-6 mb-4">
+                           <p class="title-3">Última senha chamada:</p>
+                           @if(empty($lastTicket))
+                              <p class="text-slate-400 my-4">Não existe senha anterior</p>
+                           @else 
+                              <p class="text-6xl font-bold">{{ $lastTicket->queue_ticket_number }}</p>
+                           @endif
+                           </div>
+                     </div>
+
+                     <div class="w-1/4 rounded-xl border-1 border-slate-400 text-center p-4">
+                           <div class="flex gap-6 mb-4">
+                           <p class="title-3">Próxima senha:</p>
+                           @if(empty($nextTicket)) 
+                              <p class="text-slate-400 my-4">Não existe próxima senha</p>
+                           @else 
+                              <p class="text-6xl font-bold">{{ $nextTicket->queue_ticket_number }}</p>
+                           @endif
+                           </div>
+                     </div>
+
+                     <div class="flex justify-center items-center w-1/4 rounded-xl border-1 border-slate-400 text-center p-4">
+
+                         @if(empty($nextTicket))
+                            <span class="btn opacity-20 cursor-none !p-4" >Chamar</span>
+                         @else 
+                             <a href="#" class="btn !p-4 !text-4xl">Chamar</a>
+                         @endif
+                        
+                     </div>
+
+          
+               </div>
 
 
 
