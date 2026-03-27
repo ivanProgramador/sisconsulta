c388001f683cee98c2ea4738163ffc17ca46b77a
Ivan Rodrigues <ivan-guita@hotmail.com>
2026-03-27 17:47:25 -0300

    menu do administrador ajustado

--- resources/views/components/layouts/main_menu.blade.php
+++ resources/views/components/layouts/main_menu.blade.php
@@ -2,11 +2,22 @@
 
     <div class="flex gap-4 text-lg">
 
-        <a href="{{ route('home') }}" class="btn-white"><i class="fa-solid fa-house me-2"></i>Gestão de filas</a>
-        <a href="{{ route('bundles.home') }}" class="btn-white"><i class="fa-solid fa-table-list me-2"></i>Gestão de grupos de fila</a>
-        <a href="{{ route('dispenser') }}" target="_blank" class="btn-white"><i class="fa-regular fa-copy me-2"></i>Dispensador</a>
-        <a href="{{ route('queues.display') }}" target="_blank" class="btn-white"><i class="fa-solid fa-tv me-2"></i>Apresentador</a>
-        <a href="{{ route('caller.home') }}" class="btn-white"><i class="fa-solid fa-share-from-square me-2"></i>Chamador</a>
+        @if(Auth::user()->role === 'sys-admin')
+
+             <a href="{{ route('admin.home') }}" class="btn-white"><i class="fa-solid fa-house me-2"></i>Clientes</a>
+             <a href="#" class="btn-white"><i class="fa-solid fa-chart-column me-2"></i>Estatisticas</a>
+           
+        @else
+
+            <a href="{{ route('home') }}" class="btn-white"><i class="fa-solid fa-house me-2"></i>Gestão de filas</a>
+            <a href="{{ route('bundles.home') }}" class="btn-white"><i class="fa-solid fa-table-list me-2"></i>Gestão de grupos de fila</a>
+            <a href="{{ route('dispenser') }}" target="_blank" class="btn-white"><i class="fa-regular fa-copy me-2"></i>Dispensador</a>
+            <a href="{{ route('queues.display') }}" target="_blank" class="btn-white"><i class="fa-solid fa-tv me-2"></i>Apresentador</a>
+            <a href="{{ route('caller.home') }}" class="btn-white"><i class="fa-solid fa-share-from-square me-2"></i>Chamador</a>
+
+        @endif
+
+      
     </div>
 
 </div>
