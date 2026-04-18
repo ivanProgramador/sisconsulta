760f7ff526d2fa090360adce6f7783d15edc2d0b
Ivan <ivan-guita@hotmail.com>
2026-04-18 13:01:50 -0300

    validacao de formulario admin  em andamento

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -35,6 +35,41 @@ class AdminController extends Controller
 
     public function createCompanySubmit(Request $request){
 
+        //validando formulário 
+
+        $request->validate(
+            [
+                'company_logo'=>'image|mimes:jpeg,png|dimensions:width:200,heigh:200',
+                'company_name'=>'required|max:100 |unique:companies,company_name',
+                'address'=>'required|max:255',
+                'phone'=>'required|max:20',
+                'email'=>'required|email|max:255',
+                'status'=>'required|in:active,inactive',
+                'admin_email'=>'required|email|max:50|unique:users,email',
+            ],
+            [
+                'company_logo.image' => 'O arquivo enviado não é uma imagem válida',
+                'company_logo.mimes' => 'A imagem deve estar no formato JPEG ou PNG',
+                'company_logo.dimensions' => 'A imagem deve ter extamente 200x200 pixeis',
+                'company_name.required'=>'O nome da empresa é obrigatório',
+                'company_name.max'=>'O nome da empresa não pode exceder 100 caracteres',
+                'company_name.unique'=>'Já existe um empresa com esse nome',
+                'address.required'=>'O endereço é obrigatório',
+                'address.max'=>'O endereço não pode exceder 255 caracteres',
+                'phone.required'=>'O numero de telefone é obrigatório',
+                'phone.max'=>'O numero de telefone não pode exceder 20 caracteres',
+                'email.required'=>'O e-mail é obrigatório',
+                'email.email'=>'deve ser um e-mail valido',
+                'email.max'=>'O e-mail não pode execeder 100 caracteres',
+                'status.required'=>'O status é obrigatório',
+                'status.in'=>'status inválido',
+                'admin_email.required'=>'O e-mail do administrador é obrigatório',
+                'admin_email.email'=>'O e-mail do administrador deve ser uma endereço valido',
+                'admin_email.max'=>'O e-mail do administrador não deve passar dos 50 caracteres',
+                'admin_email.unique'=>'Esse e-mail já existe',
+            ]
+        );
+
         dd($request->all());
     }
 
--- resources/views/admin/create_company_frm.blade.php
+++ resources/views/admin/create_company_frm.blade.php
@@ -20,6 +20,7 @@
                 
                 <label for="company_logo" class="label">Logo do cliente</label>
                 <input type="file" name="company_logo" id="company_logo" class="input" />
+                {!! ShowValidationError('company_logo',$errors)  !!}
 
                 <p class="text-sm text-red-500 italic" id="error_message"></p>
 
@@ -34,24 +35,30 @@
 
                 <div class="mb-4">
                     <label for="company_name" class="label">Nome do cliente</label>
-                    <input type="text" name="company_name" id="company_name" class="input w-full" />
+                    <input type="text" name="company_name" id="company_name" class="input w-full" value="{{ old('company_name') }}" />
+                    {!! ShowValidationError('company_name',$errors)  !!}
+
                 </div>
 
                 <div class="mb-4">
                     <label for="address" class="label">Endereço</label>
-                    <input type="text" name="address" id="address" class="input w-full" />
+                    <input type="text" name="address" id="address" class="input w-full" value="{{ old('address')}}" />
+                    {!! ShowValidationError('address',$errors)  !!}
                 </div>
 
                 <div class="flex gap-4 mb-4">
 
                     <div class="w-1/3">
                         <label for="phone" class="label">Telefone</label>
-                        <input type="text" name="phone" id="phone" class="input w-full" />
+                        <input type="text" name="phone" id="phone" class="input w-full" value="{{ old('phone') }}" />
+                        {!! ShowValidationError('phone',$errors)  !!}
                     </div>
 
                     <div class="w-2/3">
                         <label for="email" class="label">Email</label>
-                        <input type="email" name="email" id="email" class="input w-full" />
+                        <input type="email" name="email" id="email" class="input w-full" value="{{ old('email') }}" />
+                         {!! ShowValidationError('email',$errors)  !!}
+
                     </div>
 
                 </div>
@@ -59,11 +66,15 @@
                 <div class="mb-4">
 
                     <label for="status" class="label">Estado</label>
-                    <select class="input w-1/3">
-                        <option value="active" selected>Ativo</option>
-                        <option value="inactive">Inativo</option>
-                    </select>
+                        <select class="input w-1/3" id="status">
 
+                            <option value="active" {{ old('status') === 'active' ? 'selected': '' }}>Ativo</option>
+
+                            <option value="inactive" {{ old('status') === 'inactive' ? 'selected': '' }}>Inativo</option>
+                            
+                        </select>
+
+                    {!! ShowValidationError('status',$errors)  !!}
                 </div>
 
                 <hr class="my-6">
@@ -72,7 +83,9 @@
                     <p class="text-2xl text-slate-900">Defina o usuário administrador.</p>
                     <p class="text-sm text-slate-400 mb-4">Será enviado um email com as instruções para a conclusão do registo.</p>
                     <label for="admin_email" class="label">Email do administrador cliente</label>
-                    <input type="email" name="admin_email" id="admin_email" class="input w-full" />
+                    <input type="email" name="admin_email" id="admin_email" class="input w-full" value="{{ old('admin_email') }}" />
+                     {!! ShowValidationError('admin_email',$errors)  !!}
+
                 </div>
 
                 <button type="submit" class="btn btn-primary"><i class="fa-solid fa-check me-2"></i>Criar cliente</button>
@@ -87,7 +100,7 @@
 
 <script>
 
-    document.querySelector("#company_logo").addEventListener('change',function(event){
+ /*   document.querySelector("#company_logo").addEventListener('change',function(event){
 
         const error_message = document.querySelector("#error_message");
 
@@ -159,6 +172,9 @@
         }
         
     });
+
+
+*/
 
 </script>
 
