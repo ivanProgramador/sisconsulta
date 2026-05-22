2276ae307d621c7caa6031d03474e9772ee3dfbb
ivanProgramador <ivan-guita@hotmail.com>
2026-05-21 22:01:54 -0300

    formulario de confirmação de senha pronto

--- resources/views/auth/define_password_frm.blade.php
+++ resources/views/auth/define_password_frm.blade.php
@@ -1,3 +1,56 @@
-<div>
-    <h1>formulario em construção</h1>
-</div>
+
+<x-layouts.guest-layout subtitle="{{ $subtitle }}">
+
+ <div class="flex flex-col justify-center h-screen items-center">
+
+    <div class="main-card w-1">
+
+        <div class="flex justify-center items-center mb-8">
+            <img src="{{ asset('assets/images/favicon.png') }}" class="w-10 h-10 me-2" alt="Logo">
+            <h3 class="text-3xl uppercase">{{ config('app.name') }}</h3>
+        </div>
+
+        <div class="text-center mb-8">
+            <strong>{{ $user->email }}</strong>,<br>
+             para conclusão do registro,por favor defina a sua senha.
+        </div>
+
+        <form action="{{ Route('define.password.submit')}}" method="post">
+            @csrf
+
+           
+
+            <div class="mb-4">
+                <label for="password" class="label">Definir Senha</label>
+                <input type="password" class="input w-full" id="password" name="password" placeholder="Senha" value="{{ old('password')}}">
+                 {!! ShowValidationError('password',$errors)  !!}
+                 {!! ShowServerError('password',$errors)      !!}
+            </div>
+
+
+             <div class="mb-4">
+                <label for="password_confirmation" class="label">Repetir Senha</label>
+                <input type="password" class="input w-full" id="password_confirmation" name="password_confirmation" placeholder="Senha" value="{{ old('password')}}">
+                 {!! ShowValidationError('password',$errors)  !!}
+                 {!! ShowServerError('password',$errors)      !!}
+            </div>
+
+
+            <div class="text-center mb-4">
+                <button type="submit" class="btn w-full">Definir a senha</button>
+            </div>
+
+        </form>
+
+       
+
+    </div>
+
+    <div class="flex justify-center items-center text-xs text-zinc-700 mt-4">
+        Versão <a href="#" class="link ms-2">{{ config('constants.APP_VERSION') }}</a>
+        <span class="mx-2">|</span>
+       
+    </div>
+
+   </div>
+</x-layouts.guest-layout>
