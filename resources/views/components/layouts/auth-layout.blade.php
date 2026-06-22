97ef91f48bdb194eeb492821d4c138897f427b6e
ivanProgramador <ivan-guita@hotmail.com>
2026-06-22 16:34:47 -0300

    integracao com apex em andamento

--- resources/views/components/layouts/auth-layout.blade.php
+++ resources/views/components/layouts/auth-layout.blade.php
@@ -16,6 +16,10 @@
     <link rel="stylesheet" href="{{ asset('assets/coloris/coloris.min.css') }}">
     <script src="{{ asset('assets/coloris/coloris.min.js') }}"></script>
 
+    {{-- apex charts --}}
+    <script src="{{ asset('assets/apexcharts/apexcharts.js') }}"></script>
+
+
     
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
